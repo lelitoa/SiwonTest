471b8f5f83a5221355a587a784446eb130efaf02
Adam Lelito <adam.lelito@microsoft.wsei.edu.pl>
2023-04-20 13:47:17 +0200

    Part 4 to fix
    com.android.volley problem

--- app/src/main/java/com/example/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapiapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.weatherapiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,10 +42,67 @@ public class MainActivity extends AppCompatActivity {
         lv_weatherReport = findViewById(R.id.lv_weatherReports);
 
         // click listeners for each button
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://www.metaweather.com/api/location/search/?query=" + et_dataInput.getText().toString();
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>()
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                    try {
+                        JSONObject cityInfo = response.getJSONObject(0);
+                        String cityID = cityInfo.getString("woeid");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(MainActivity.this, "City ID = " + cityID, Toast.LENGTH_SHORT).show();
+                }, new Respnse.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                    Toast.makeText(MainActivity.this, "Something wrong.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                queue.add(request);
+
+//                // Request a string response form the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                                }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+                // Add the request to the RequestQueue.
+//                queue.add(stringRequest);
+
+//                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(MainActivity)
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
